99eba0c162bf8ff8ae6f05e1cb6c3c632853bae5
vlad <elphynkarelov@gmail.com>
2025-11-26 19:19:16 +0900

    Changes to vite.config again

--- src/web/vite.config.js
+++ src/web/vite.config.js
@@ -1,28 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => {
-  if (command === "serve") {
-    return {
-      // without this on build absolute paths ruin access to js,css files in ./assets
-      base: "./",
-      plugins: [
-        react({
-          babel: {
-            plugins: [["babel-plugin-react-compiler"]],
-          },
-        }),
-      ],
-    };
-  } else {
-    return {
-      plugins: [
-        react({
-          babel: {
-            plugins: [["babel-plugin-react-compiler"]],
-          },
-        }),
-      ],
-    };
-  }
+export default defineConfig({
+  // without this on build absolute paths ruin access to js,css files in ./assets
+  base: "./",
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler"]],
+      },
+    }),
+  ],
 });
